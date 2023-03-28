88d8f0b3d0ce80b176c8996b9daa0d4102363ef0
Yoo-uijin <timetam24@naver.com>
2023-03-29 08:45:01 +0900

    Update Edit.js

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,36 +1,10 @@
 import React from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Edit = () => {
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
 
-  const id = searchParams.get("id");
-  const mode = searchParams.get("mode");
-
-  return (
-    <div>
-      <h1>Edit</h1>
-      <p>이 곳은 일기 수정 페이지입니다.</p>
-      <button onClick={() => setSearchParams({ what: "hungry" })}>
-        QS 바꾸기
-      </button>
-      <button
-        onClick={() => {
-          navigate("/home");
-        }}
-      >
-        HOME으로 이동
-      </button>
-      <button
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        뒤로 가기
-      </button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Edit;
